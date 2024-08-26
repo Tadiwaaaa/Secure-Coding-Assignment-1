5944532239cad39b4510818b5f5bc58a1b210a71
Tadiwaaaa <164057671+Tadiwaaaa@users.noreply.github.com>
2024-08-26 22:26:41 +0200

    Add files via upload
    Number 6: I created a class Insurance that inherits from the Bank class.
    Number 7: I created a method cover that prints the statement “You are covered”.

---
+++ Insurance.java
@@ -0,0 +1,10 @@
+package Assignment_1;
+// inherit from class Bank
+public class Insurance extends Bank {
+    public Insurance(String accountType, double accountBalance) {
+        super(accountType, accountBalance);
+    }
+    public void cover() {
+        System.out.println("You are covered");
+    }
+}
